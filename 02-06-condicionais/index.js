8888f084fc08551cecd87092099a742f46348160
Kleberson Santos <Kleberson2Santos@gmail.com>
2023-03-10 16:48:30 -0400

    condicionais

---
+++ 02-06-condicionais/index.js
@@ -0,0 +1,16 @@
+var me = {
+  name: 'John',
+  age: 17,
+};
+
+function checkAge(person) {
+  console.log('A idade da pessoa é: ' + person.age);
+
+  if (person.age >= 18) {
+    console.log('A pessoa é maior de idade');
+  } else {
+    console.log('A pessoa é menor de idade');
+  }
+}
+
+checkAge(me);
