999ea7f656385b411acdd69114eee00481c8a414
Kleberson Santos <Kleberson2Santos@gmail.com>
2023-03-22 19:01:40 -0400

    fundamentos do javascript

---
+++ 03-02-tudo-e-objeto/index.js
@@ -0,0 +1,10 @@
+//console
+'boo'.length;
+var me = { name: 'john' };
+me.name(100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+null.prototype;
+undefined.prototype;
+String.prototype;
+Number.prototype;
+Boolean.prototype;
